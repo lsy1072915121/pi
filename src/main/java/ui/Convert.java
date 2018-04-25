9821c32a39e39c1350d4626186aff81f93457623
liushiyao <dirk454587963>
2018-04-25 18:20:48 +0800

    v1.0

---
+++ src/main/java/ui/Convert.java
@@ -0,0 +1,44 @@
+package ui;
+
+public class Convert {
+
+  private String status;
+
+  private String info;
+
+  private String infocode;
+
+  private String locations;
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getInfo() {
+    return info;
+  }
+
+  public void setInfo(String info) {
+    this.info = info;
+  }
+
+  public String getInfocode() {
+    return infocode;
+  }
+
+  public void setInfocode(String infocode) {
+    this.infocode = infocode;
+  }
+
+  public String getLocations() {
+    return locations;
+  }
+
+  public void setLocations(String locations) {
+    this.locations = locations;
+  }
+}
